f9feaf79c13a9de53002f1ec2fd579d350468b89
Mehmet-fth <mehmetfatiholcan@gmail.com>
2025-05-24 18:43:38 +0300

    Create veri.js

---
+++ veri.js
@@ -0,0 +1,18 @@
+const icerikler = {
+  ayetler: [
+    "“Hiç bilenlerle bilmeyenler bir olur mu?” (Zümer 9)",
+    "“Allah sabredenlerle beraberdir.” (Bakara 153)"
+  ],
+  hadisler: [
+    "“Kolaylaştırın, zorlaştırmayın; müjdeleyin, nefret ettirmeyin.” (Buhârî)",
+    "“Komşusu açken tok yatan bizden değildir.” (Müslim)"
+  ],
+  dualar: [
+    "“Rabbim! Bana katından bir rahmet ver...” (Kehf 10)",
+    "“Ey kalpleri evirip çeviren Allah’ım, kalbimi dinin üzere sabit kıl.”"
+  ],
+  tavsiyeler: [
+    "Bugün bir kişiye selam ver. Gönlünü fethet.",
+    "Telefonunu bir süre kapat, Kur’an’la vakit geçir."
+  ]
+};
